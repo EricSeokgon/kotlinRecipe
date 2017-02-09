c8c1060b62f65da30922be216e0525ce8dd8703f
sklee <hadesleeq@gmail.com>
2017-02-09 17:48:35 +0900

    Initial commit

---
+++ src/app.kt
@@ -0,0 +1,13 @@
+/**
+ * Project: kotlinRecipe
+ * FileName: app
+ * Date: 2017-02-09
+ * Time: 오후 5:43
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+fun main(args: Array<String>) {
+    print("Hello, World!")
+}
+
