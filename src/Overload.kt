b200db2543b2666a4283be4dde8803d03a805fe0
sklee <hadesleeq@gmail.com>
2017-10-16 14:35:10 +0900

    산술연산자

---
+++ src/Overload.kt
@@ -0,0 +1,20 @@
+/**
+ * Project: kotlinRecipe
+ * FileName: Overload
+ * Date: 2017-10-16
+ * Time: 오후 2:23
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+fun main(args: Array<String>) {
+    val m1 = Score(100, 200)
+    val m2 = Score(300, 400)
+    println(m1 + m2)
+}
+
+data class Score(val a: Int, val b: Int) {
+    operator fun plus(other: Score): Score {
+        return Score(a + other.a, b + other.b)
+    }
+}
